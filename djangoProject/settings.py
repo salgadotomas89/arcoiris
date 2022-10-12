58f85870cfed8c439ce74e716c0877384742d9bf
salgadotomas89 <salgadotomas@outlook.com>
2022-10-12 20:26:54 -0300

    add railway

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-kqkg^@&cc0@ic4zdks(%n+x&=bb94(kr4g$l&y8qw-#3t4(!wz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['escuela-arcoiris.herokuapp.com']
+ALLOWED_HOSTS = ['escuela-arcoiris.herokuapp.com', 'web-production-3060.up.railway.app']
 
 
 # Application definition
