f3d26d9adebe88df7386030b0034302b68e1804f
salgadotomas89 <salgadotomas@outlook.com>
2022-10-12 22:21:01 -0300

    add library whitenoise

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-kqkg^@&cc0@ic4zdks(%n+x&=bb94(kr4g$l&y8qw-#3t4(!wz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['escuela-arcoiris.herokuapp.com', 'web-production-3060.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1','escuela-arcoiris.herokuapp.com', 'web-production-3060.up.railway.app']
 
 
 # Application definition
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+        "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,11 +114,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [    os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
